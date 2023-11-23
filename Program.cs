f45c0cff0700ef7f227bb3cf2e0c2dff1261451d
Vojta <vojtech.elman1@gmail.com>
2023-11-23 10:56:56 +0100

    Initial commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+bool proceed = true;
+string finalSentence = "";
+while (proceed)
+{
+    Console.WriteLine("Zadej slovo");
+    string userInputA = Console.ReadLine();
+
+    if (string.IsNullOrWhiteSpace(userInputA))
+    {
+        Console.WriteLine(finalSentence);
+        proceed = false;
+    }
+    else if (!string.IsNullOrEmpty(userInputA))
+    {
+        finalSentence += userInputA;
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
